7334929dca76c71a54c22066889ec050cd1614cf
Burak Demir <burakdemirafl@gmail.com>
2022-05-18 20:14:59 +0300

    Coffee Show Application

---
+++ app/src/main/java/com/burakdemir/coffeeshow/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.burakdemir.coffeeshow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var counter=0 //holds number for coffee index
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        changeImage(counter)
+    }
+
+    fun forward(view: View) {
+        counter++;
+        counter%=4;
+        changeImage(counter)
+    }
+    fun backward(view: View){
+        counter--;
+        if(counter<0)
+            counter+=4;
+        counter%=4;
+        changeImage(counter)
+    }
+    fun changeImage(counter:Int){
+        when(counter){
+            0-> {
+                coffeeImages.setImageResource(R.drawable.americano)
+                coffeeName.text = "Americano"
+                backward.text="< Turkish Coffee"
+                forward.text="Espresso >"
+            }
+            1-> {
+                coffeeImages.setImageResource(R.drawable.espresso)
+                coffeeName.text = "Espresso"
+                backward.text="< Americano"
+                forward.text="Filter Coffee >"
+            }
+            2-> {
+                coffeeImages.setImageResource(R.drawable.filter)
+                coffeeName.text = "Filter Coffee"
+                backward.text="< Espresso"
+                forward.text="Turkish Coffee >"
+            }
+            3-> {
+                coffeeImages.setImageResource(R.drawable.turkish)
+                coffeeName.text = "Turkish Coffee"
+                backward.text="< Filter Coffee"
+                forward.text="Americano >"
+            }
+        }
+    }
+}
